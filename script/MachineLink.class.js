27d935900de8c673809f104b01ca8f92a643b20d
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-13 20:01:03 +0200

    Implement MachineLink class

--- script/MachineLink.class.js
+++ script/MachineLink.class.js
@@ -1,6 +1,27 @@
 class MachineLink {
 
-    constructor() {
-        this.name = 'juhu';
+    constructor(startState) {
+        this.startState = startState;
+        this.pointList = Array();
+        this.pointList.push({x: startState.getX(), y: startState.getY()});
+    }
+
+    addEndState(endState){
+        this.endState = endState;
+        this.pointList.push({x: endState.getX(), y: endState.getY()});
+    }
+
+    addPoint(pointX, pointY) {
+        this.pointList.push({x: pointX, y: pointY});
+    }
+
+    draw() {
+        Canvas.context.beginPath();
+        Canvas.context.moveTo(this.pointList.x, this.pointList.y);
+        for(let i = 0; i < this.pointList.length; i++) {
+            Canvas.context.lineTo(this.pointList[i].x, this.pointList[i].y);
+        }
+        
+        Canvas.context.stroke();
     }
 }

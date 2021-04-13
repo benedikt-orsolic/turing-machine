e03cbf20b15f2a3b8540ee6b69874621dc3db72b
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-13 18:50:47 +0200

    State list works

--- script/main.js
+++ script/main.js
@@ -1,18 +1,31 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
 const stateList = Array();
+windowResizeEvent();
+animate();
 
-windowResizeEvent()
 
 
 document.getElementById('canvas').addEventListener('click', function(event){
+    
     stateList.push(new State(event.clientX, event.clientY));
-    console.log(stateList)
+    
 });
-window.addEventListener('resize', windowResizeEvent)
+window.addEventListener('resize', windowResizeEvent);
+
+
+
+function animate() {
+
+    stateList.forEach(function(state){
+        state.draw();
+    });
+
+    
+    requestAnimationFrame(animate);
+}
+
+
 
 function windowResizeEvent() {
-    canvas.height = window.innerHeight
-    canvas.width = window.innerWidth
+    Canvas.canvas.height = window.innerHeight
+    Canvas.canvas.width = window.innerWidth
 }

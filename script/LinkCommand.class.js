0e252123079fcac97a9de25068c2320c125c989f
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-14 21:21:00 +0200

    Move default direciton before ifs so it doesn't override them

--- script/LinkCommand.class.js
+++ script/LinkCommand.class.js
@@ -4,14 +4,14 @@ class LinkCommand {
         this.wantInCell = document.getElementById("wantInCell").value;
         this.putInCell = document.getElementById("putInCell").value;
         
+            
+        this.direction = 'S';
         if(document.getElementById("directionLeft").checked){
             this.direction = 'L';
         }
         if(document.getElementById("directionRight").checked){
             this.direction = 'R';
         }
-        
-        this.direction = 'S';
     }
 
 

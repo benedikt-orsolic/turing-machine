09bffcb4b6c0dcd7dfa786ef35dff2e942be753f
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-14 16:47:33 +0200

    Create LinkCommand class

---
+++ script/LinkCommand.class.js
@@ -0,0 +1,8 @@
+class LinkCommand {
+
+    constructor(wantOnLine, putOnLine, direction) {
+        this.wantOnLine = wantOnLine;
+        this.putOnLine = putOnLine;
+        this.direction = direction;
+    }
+}

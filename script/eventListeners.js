a6811d3a4e0daef3e60bbf6f23b65af74fb78847
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-14 21:16:38 +0200

    Add next step button chnage name of if button click on canvas funciton

--- script/eventListeners.js
+++ script/eventListeners.js
@@ -1,5 +1,6 @@
-document.getElementById('canvas').addEventListener(         'click', function(event){ mouseClicked(event)        });
+document.getElementById('canvas').addEventListener(         'click', function(event){ mouseClickedOnCanvas(event)        });
 document.getElementById('addState').addEventListener(       'click', function(){ control = 'addState';           });
 document.getElementById('addLink').addEventListener(        'click', function(){ control = 'addLink';            });
+document.getElementById('nextStep').addEventListener(       'click', function(){ machine.resolveLink();           });
 
 window.addEventListener('resize', windowResizeEvent);

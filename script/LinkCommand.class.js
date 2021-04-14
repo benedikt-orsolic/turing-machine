0692a14c98bdbc640b66e19c412d9cdb488f6fde
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-14 17:05:01 +0200

    Use better names

--- script/LinkCommand.class.js
+++ script/LinkCommand.class.js
@@ -1,8 +1,16 @@
 class LinkCommand {
 
-    constructor(wantOnLine, putOnLine, direction) {
-        this.wantOnLine = wantOnLine;
-        this.putOnLine = putOnLine;
-        this.direction = direction;
+    constructor() {
+        this.wantInCell = document.getElementById("wantInCell").value;
+        this.putInCell = document.getElementById("putInCell").value;
+        
+        if(document.getElementById("directionLeft").checked){
+            this.direction = 'L';
+        }
+        if(document.getElementById("directionRight").checked){
+            this.direction = 'R';
+        }
+        
+        this.direction = 'S';
     }
 }

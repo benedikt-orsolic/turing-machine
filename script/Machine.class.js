e020b805bf9429b19ac28d7e65762209ce7c95d5
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-14 21:38:04 +0200

    Machine can execute links

--- script/Machine.class.js
+++ script/Machine.class.js
@@ -8,10 +8,44 @@ class Machine{
         this.stateList.push(newState);
     }
 
+    resolveLink() {
+        let activatedLink = this.currentState.getActivatedLink(this.currentTapeCell);
+        let command = activatedLink.getCommand();
+
+        this.currentTapeCell = command.getPutInCell();
+        if(command.getDirection() == 'L'){
+
+            if(this.machineTapeStrLeft == '') this.machineTapeStrLeft = 'a';
+            this.machineTapeStrRight = this.currentTapeCell + this.machineTapeStrRight;
+            this.currentTapeCell = this.machineTapeStrLeft.substring(this.machineTapeStrLeft.length - 1, this.machineTapeStrLeft.length);
+            this.machineTapeStrLeft = this.machineTapeStrLeft.substring(this.machineTapeStrLeft.length - 1);
+        }
+
+        if(command.getDirection() == 'R'){
+            
+            if(this.machineTapeStrRight == '') this.machineTapeStrRight = 'a';
+            this.machineTapeStrLeft = this.machineTapeStrLeft + this.currentTapeCell;
+            this.currentTapeCell = this.machineTapeStrRight.substring(0, 1);
+            this.machineTapeStrRight = this.machineTapeStrRight.substring(1, this.machineTapeStrLeft.length);
+        }
+
+        this.currentState = activatedLink.getEndState();
+        this.updateMachineTape();
+    }
+
     draw() {
+
+        
+
         this.stateList.forEach(function(state){
             state.draw();
         });
+    }
+
+    updateMachineTape() {
+        document.getElementById('machineTapeLeft').value = this.machineTapeStrLeft;
+        document.getElementById('machineTapeRight').value = this.machineTapeStrRight;
+        document.getElementById('machineTapeCurrentCell').value = this.currentTapeCell;
     }
 
     getSelectedState(x, y) {
